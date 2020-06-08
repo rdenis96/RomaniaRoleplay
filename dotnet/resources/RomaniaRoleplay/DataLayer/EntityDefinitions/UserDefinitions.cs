4e06fac9030653c594face97634bff0f84309256
Radu Denis Florian <rdenis9616@gmail.com>
2020-06-08 20:36:36 +0300

    Create mysql connection for users, create user class, configure meta.xml for specific environment.

---
+++ dotnet/resources/RomaniaRoleplay/DataLayer/EntityDefinitions/UserDefinitions.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Users.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer.EntityDefinitions
+{
+    internal static class UserDefinitions
+    {
+        public static void Set(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(p => p.Id);
+            modelBuilder.Entity<User>().Property(p => p.Username).IsRequired();
+            modelBuilder.Entity<User>().Property(p => p.Email).IsRequired();
+            modelBuilder.Entity<User>().Property(p => p.Password).IsRequired();
+            modelBuilder.Entity<User>().Property(p => p.RegisterDate).IsRequired().HasColumnType("datetime");
+            modelBuilder.Entity<User>().Property(p => p.LastActiveDate).IsRequired().HasColumnType("datetime");
+        }
+    }
+}
