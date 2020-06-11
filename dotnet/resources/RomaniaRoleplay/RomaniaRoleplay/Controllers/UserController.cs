a472bb95783621e2f839f91280987efbe558361f
Radu Denis Florian <rdenis9616@gmail.com>
2020-06-11 16:42:21 +0300

    Update user login active date

--- dotnet/resources/RomaniaRoleplay/RomaniaRoleplay/Controllers/UserController.cs
+++ dotnet/resources/RomaniaRoleplay/RomaniaRoleplay/Controllers/UserController.cs
@@ -1,7 +1,9 @@
-﻿using GTANetworkAPI;
+﻿using Domain.Users.Models;
+using GTANetworkAPI;
 using Helper.Common;
 using Newtonsoft.Json;
 using RomaniaRoleplay.Models.Login;
+using System;
 
 namespace RomaniaRoleplay.Controllers
 {
@@ -17,11 +19,18 @@ namespace RomaniaRoleplay.Controllers
             var dbPlayer = _usersWorker.GetPlayerInfoByUsernameAndPassword(loginModel.Username, encryptedPass);
             if (dbPlayer != null)
             {
+                SetUserDetailsOnSignIn(player, dbPlayer);
+                _usersWorker.Update(dbPlayer);
+
                 player.SetSkin(PedHash.Swat01SMY); //for testing, will be removed
                 NAPI.Player.SpawnPlayer(player, new Vector3(-1036.755f, -2737.948f, 21.2772f));
             }
             player.TriggerEvent("onUserLoginResponse", dbPlayer);
 
+        }
+        private void SetUserDetailsOnSignIn(Player player, User user)
+        {
+            user.LastActiveDate = DateTime.UtcNow;
         }
     }
 }
